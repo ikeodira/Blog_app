0e58bc62bf6b833c0d0d38d146d55135cd779f3f
Ike Odira <odiraike8@gmail.com>
2024-03-06 03:28:07 -0500

    made changes to the search screen page

--- lib/inner_screens/search_screen.dart
+++ lib/inner_screens/search_screen.dart
@@ -40,97 +40,99 @@ class _SearchScreenState extends State<SearchScreen> {
     final Color color = Utils(context).getColor;
 
     return SafeArea(
-      child: Scaffold(
-        body: GestureDetector(
-          onTap: () {
-            FocusScope.of(context).unfocus();
-          },
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        focusNode.unfocus();
-                        Navigator.pop(context);
-                      },
-                      child: const Icon(IconlyLight.arrowLeft2),
-                    ),
-                    Flexible(
-                      child: TextField(
-                        focusNode: focusNode,
-                        controller: _searchTextController,
-                        style: TextStyle(color: color),
-                        autofocus: true,
-                        textInputAction: TextInputAction.search,
-                        keyboardType: TextInputType.text,
-                        onEditingComplete: () {},
-                        decoration: InputDecoration(
-                          contentPadding: const EdgeInsets.only(
-                            bottom: 8 / 5,
-                          ),
-                          hintText: "Search",
-                          enabledBorder: InputBorder.none,
-                          focusedBorder: InputBorder.none,
-                          suffix: Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 5),
-                            child: GestureDetector(
-                              onTap: () {
-                                _searchTextController.clear();
-                                focusNode.unfocus();
-                                setState(() {});
-                              },
-                              child: const Icon(
-                                Icons.close,
-                                size: 18,
-                                color: Colors.red,
+      child: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Scaffold(
+          body: Expanded(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          focusNode.unfocus();
+                          Navigator.pop(context);
+                        },
+                        child: const Icon(IconlyLight.arrowLeft2),
+                      ),
+                      Expanded(
+                        child: TextField(
+                          focusNode: focusNode,
+                          controller: _searchTextController,
+                          style: TextStyle(color: color),
+                          autofocus: true,
+                          textInputAction: TextInputAction.search,
+                          keyboardType: TextInputType.text,
+                          onEditingComplete: () {},
+                          decoration: InputDecoration(
+                            contentPadding: const EdgeInsets.only(
+                              bottom: 8 / 5,
+                            ),
+                            hintText: "Search",
+                            enabledBorder: InputBorder.none,
+                            focusedBorder: InputBorder.none,
+                            suffix: Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 5),
+                              child: InkWell(
+                                onTap: () {
+                                  _searchTextController.clear();
+                                  focusNode.unfocus();
+                                  setState(() {});
+                                },
+                                child: const Icon(
+                                  Icons.close,
+                                  size: 18,
+                                  color: Colors.red,
+                                ),
                               ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ),
-              const VerticalSpacing(20),
-              Flexible(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: MasonryGridView.count(
-                    itemCount: searchKeywords.length,
-                    crossAxisCount: 4,
-                    mainAxisSpacing: 4,
-                    crossAxisSpacing: 4,
-                    itemBuilder: (context, index) {
-                      return InkWell(
-                        onTap: () {},
-                        child: Container(
-                          margin: const EdgeInsets.all(4.0),
-                          decoration: BoxDecoration(
-                            border: Border.all(color: color),
-                            borderRadius: BorderRadius.circular(30),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              searchKeywords[index],
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                        ),
-                      );
-                    },
+                    ],
                   ),
                 ),
-              ),
-              const EmptyNewsWidget(
-                text: "Oops! No results found",
-                imagePath: "assets/images/search.png",
-              ),
-            ],
+                const VerticalSpacing(20),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: MasonryGridView.count(
+                      itemCount: searchKeywords.length,
+                      crossAxisCount: 4,
+                      mainAxisSpacing: 4,
+                      crossAxisSpacing: 4,
+                      itemBuilder: (context, index) {
+                        return GestureDetector(
+                          onTap: () {},
+                          child: Container(
+                            margin: const EdgeInsets.all(4.0),
+                            decoration: BoxDecoration(
+                              border: Border.all(color: color),
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                searchKeywords[index],
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+                const EmptyNewsWidget(
+                  text: "Oops! No results found",
+                  imagePath: "assets/images/search.png",
+                ),
+              ],
+            ),
           ),
         ),
       ),
